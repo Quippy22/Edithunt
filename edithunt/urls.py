757616e481f1f3bca4f2c02cb12e281467303dc0
darius alexandru <darius.alexandru2202@gmail.com>
2025-11-21 12:35:40 +0200

    docs: document database models and fix general code formatting

--- edithunt/urls.py
+++ edithunt/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("core.urls")),
+    path("", include("core.urls")), # Routes the root URL to the core application's URLs.
 ]
